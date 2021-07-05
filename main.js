f1bb84e9f6e500eeb3534e107bf326be3f26e82e
abel0011 <abellpazz12@gmail.com>
2021-07-04 20:10:27 -0500

    se ha generado typed

---
+++ main.js
@@ -0,0 +1,80 @@
+var lst=['programador','diseñador','analista','si chicho'];
+var content=document.getElementById('palabras');
+var contentPalabra=content.innerHTML;
+//condicion para agregar palabra
+var addcontent=false;
+//contador
+var x=0;
+
+var lstabel=['programador','diseñador','analista','si chicho'];
+var contentabel=document.getElementById('pabel');
+var contentPalabraabel=contentabel.innerHTML;
+//condicion para agregar palabra
+var addcontentabel=false;
+//contador
+var xabel=0;
+
+
+
+
+
+setInterval(function(){
+     if(contentPalabra.length > 0 && !addcontent){
+          content.innerHTML=contentPalabra.slice(0,-1);
+     contentPalabra=content.innerHTML;
+     }
+     else{
+          addcontent=true;
+     }
+
+
+     //agregando palabras (seteando)
+     if(addcontent){
+          if(contentPalabra.length < lst[x].length){
+               content.innerHTML=lst[x].slice(0,contentPalabra.length+1);
+               contentPalabra=content.innerHTML;
+          }
+          else{
+               if(x < lst.length){
+                    x++;
+               }
+               addcontent=false;
+
+          }
+     }
+     if(x == lst.length){
+     x=0;
+     }
+if(contentPalabraabel.length > 0 && !addcontentabel){
+          contentabel.innerHTML=contentPalabraabel.slice(0,-1);
+     contentPalabraabel=contentabel.innerHTML;
+     }
+     else{
+          addcontentabel=true;
+     }
+
+
+     //agregando palabras (seteando)
+     if(addcontentabel){
+          if(contentPalabraabel.length < lst[x].length){
+               contentabel.innerHTML=lst[x].slice(0,contentPalabraabel.length+1);
+               contentPalabraabel=contentabel.innerHTML;
+          }
+          else{
+               if(xabel< lstabel.length){
+                    xabel++;
+               }
+               addcontentabel=false;
+
+          }
+     }
+     if(xabel== lstabel.length){
+     xabel=0;
+     }
+
+},70);
+
+     
+
+
+
