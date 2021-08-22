f9c4f3766a9912a8aafbca3c5e2c8455242a7bc7
Abel Paz <abellpazz12@gmai.com>
2021-08-21 22:35:43 -0500

    generando colores

--- js/main.js
+++ js/main.js
@@ -3,12 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
     toggleMenu = document.getElementById("toggle-menu"),
     toggleBar = document.getElementById("toggle-bar"),
     headerElement = document.getElementById("header"),
-    mainElement = document.getElementById("main");
+    mainElement = document.getElementById("main"),
+    arrowUp = document.getElementById("arrow");
+
   toggleMenu.addEventListener("click", () => {
     toggleBar.classList.toggle("active"),
       mainNav.classList.toggle("main-nav--show"),
       header.classList.toggle("blur"),
       main.classList.toggle("blur"),
       document.body.classList.toggle("no-scroll");
+  });
+
+  window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+    const scrollable =
+      document.documentElement.scrollHeight - window.innerHeight;
+
+    if (Math.ceil(scrolled) === scrollable) {
+      console.log("has llegado al final");
+    }
+  });
+
+  arrowUp.addEventListener("click", () => {
+    window.scrollTo(0, document.querySelector(".arrow").scrollHeight);
   });
 });
